ddaa785d3fa7364c18b914a0dfde1fdf7f0aae37
Priscilla Normando <priscillanormando@gmail.com>
2020-08-14 04:37:41 +0000

    Código com GET, PUT, PUSH, DEL.

--- src/app.js
+++ src/app.js
@@ -1,39 +1,54 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+function validateRepositoryId(request, response, next) {
+	const { id } = request.params;
+
+	if(!isUuid(id)){
+		return response.status(400).json({
+			error: 'Invalid exemple ID.'
+		})
+	}
+	return next();
+}
+
+app.use('/repositories/:id', validateRepositoryId);
 
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
   const { title } = request.query;
 	const results = title
-		? repositories.filter(repository => repository.title.includes(title))
+		? repositories.filter(repository =>  repository.title.includes(title))
 		: repositories;
 	return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  const {title, owner} = request.body;
-	const repository = {id: uuid(), title, owner };
+  const {title, url, techs} = request.body;
+	const repository = {id: uuid(), title, url, techs, like:0};
 	repositories.push(repository);
 	return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const {id} = request.params;
-	const repositoryIndex = repositories.findIndex(repositories => repository.id === id);
+  const { title, url, techs } = request.body;
+	const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 	if (repositoryIndex < 0){
 		return response.status(400).json({error: 'Repository not found'})
 	}
 	const repository = {
 		id,
 		title,
-		owner,
-	}
+		url,
+    techs,
+	};
 	repositories[repositoryIndex] = repository;
 	return response.json(repository);
 });
@@ -49,8 +64,16 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  const {title, owner} = request.body;
-	const repository = {id: uuid(), title, owner };
+  const {id} = request.params;
+	const repositoryIndex = repositories.findIndex(repository.id === id);
+  if (repositoryIndex < 0){
+		return response.status(400).json({error: 'Repository not found'})
+	}
+  const repository = {
+    ...repositories[repositoryIndex],
+    likes: ++repositories[repositoryIndex].likes
+  };
+  repositories[repositoryIndex] = repository;
 	repositories.push(repository);
 	return response.json(repository);
 });
