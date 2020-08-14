3e7665fac57fa340915ab33b7afc0aa7baa4d318
Priscilla Normando <priscillanormando@gmail.com>
2020-08-14 04:07:50 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
-
+const { uuid } = require("uuidv4");
 const app = express();
 
 app.use(express.json());
@@ -11,23 +9,50 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query;
+	const results = title
+		? repositories.filter(repository => repository.title.includes(title))
+		: repositories;
+	return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, owner} = request.body;
+	const repository = {id: uuid(), title, owner };
+	repositories.push(repository);
+	return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+	const repositoryIndex = repositories.findIndex(repositories => repository.id === id);
+	if (repositoryIndex < 0){
+		return response.status(400).json({error: 'Repository not found'})
+	}
+	const repository = {
+		id,
+		title,
+		owner,
+	}
+	repositories[repositoryIndex] = repository;
+	return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+	const repositoryIndex = repositories.findIndex(repositories => repository.id === id);
+	if (repositoryIndex < 0){
+		return response.status(400).json({error: 'Repository not found'})
+	}
+	repositories.splice(repositoryIndex, 1);
+	return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {title, owner} = request.body;
+	const repository = {id: uuid(), title, owner };
+	repositories.push(repository);
+	return response.json(repository);
 });
 
 module.exports = app;
