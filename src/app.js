f0fffdf44fb32446c7dcd05c2a7b41fb64151ef4
Priscilla Normando <priscillanormando@gmail.com>
2021-01-02 21:47:13 +0000

    Método get atualizado e método post corrigido.

--- src/app.js
+++ src/app.js
@@ -22,16 +22,12 @@ app.use('/repositories/:id', validateRepositoryId);
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  const { title } = request.query;
-	const results = title
-		? repositories.filter(repository =>  repository.title.includes(title))
-		: repositories;
 	return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   const {title, url, techs} = request.body;
-	const repository = {id: uuid(), title, url, techs, like:0};
+	const repository = {id: uuid(), title, url, techs, likes:0};
 	repositories.push(repository);
 	return response.json(repository);
 });
