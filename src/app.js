947c6c04bef15763c9e03c2f6ce8a987dddcb421
Priscilla Normando <priscillanormando@gmail.com>
2021-01-02 21:55:23 +0000

    Rota delete corrigida.

--- src/app.js
+++ src/app.js
@@ -51,12 +51,13 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
-	const repositoryIndex = repositories.findIndex(repositories => repository.id === id);
-	if (repositoryIndex < 0){
-		return response.status(400).json({error: 'Repository not found'})
-	}
-	repositories.splice(repositoryIndex, 1);
-	return response.status(204).send();
+	const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+	if (findRepositoryIndex >= 0){
+		repositories.splice(findRepositoryIndex, 1);
+	  return response.status(204).send();
+	}else{
+    return response.status(400).json({error: 'Repository not found'})
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
